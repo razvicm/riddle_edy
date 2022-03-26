e042b7f6391bf558f7e8d61cd973eae77757539f
Razvan <rciupei@yahoo.com>
2022-03-26 14:37:44 +0200

    typo

--- pages/challenges/Chapter6/9.js
+++ pages/challenges/Chapter6/9.js
@@ -11,7 +11,7 @@ function Chapter1() {
       <Text h3>Capitolul 6</Text>
       <Spacer />
       <Text>
-        Dupa ce am reusit sa descifrez nota muzicala si sa gesesc procesoarele
+        Dupa ce am reusit sa descifrez nota muzicala si sa gasesc procesoarele
         corecte si combinand cele trei forme geometrice am avut si codul de
         repornire. Dupa ce sistemul din nou era online am reusit sa las si
         mesajul SOS. Apoi am parasit epava si ne-am indreptat rapid spre naveta
