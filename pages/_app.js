8bcfc4594ef3cb9e53d0737609b074b36cc60ecb
Razvan <rciupei@yahoo.com>
2024-04-17 22:14:47 +0300

    add meta

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,16 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Rejtveny</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="European Travel Destinations" />
+        <meta property="og:type" content="article" />
+        <meta
+          property="og:image"
+          content="https://images.unsplash.com/photo-1713189005053-e38b1b88ac4a?q=80&w=1958&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        />
+        <meta
+          property="og:url"
+          content="http://euro-travel-example.com/index.htm"
+        />
       </Head>
       <Layout>
         <Component {...pageProps} />
